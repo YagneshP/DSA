f0f33a839aaebe81be6b214018d9103245abce8e
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-04-27 07:26:06 -0400

    linked-list-values iterative way

--- linked-list-values.js
+++ linked-list-values.js
@@ -1,4 +1,48 @@
 /** Problem
  * Return the Array with values which contains in each nodes of linked list [ Singally linked list]
+ */
+
+/**
+ * Approach
+ *
+ * travsal -> Need a variable / pointer , Time = O(n) Space = O(n)
  *
  */
+
+//create Node class
+
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
+}
+
+const a = new Node("A");
+const b = new Node("B");
+const c = new Node("C");
+const d = new Node("D");
+
+//create Linked list
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+//iterative
+
+const linkedListValues = (head) => {
+  // need an array to store the values of node
+  let values = [];
+  let current = head; //start assign head to a variable
+
+  while (current !== null) {
+    //need to add node value to array
+    values.push(current.value);
+    //assign current to current.next
+    current = current.next;
+  }
+  return values;
+};
+
+console.log(linkedListValues(a));
